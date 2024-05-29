46bb35b2c773bf10b5dc9e32265bfb21afdf1eb5
zozulia.kirill <kirillzozulya95@gmail.com>
2024-05-29 10:36:20 +0300

    make build

---
+++ index.15ebfeb1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("thead tr"),r=e.children,o=r[1].cloneNode(!0);e.insertBefore(o,r[r.length-1]),document.querySelectorAll("tbody tr").forEach(function(e){var r=e.children,o=r[1].cloneNode(!0);e.insertBefore(o,r[r.length-1])});var t=document.querySelector("tfoot tr"),n=t.children,c=n[1].cloneNode(1);t.insertBefore(c,n[n.length-1]);
+//# sourceMappingURL=index.15ebfeb1.js.map
